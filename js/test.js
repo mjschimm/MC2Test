e47b15006f2d34d4ba70f268708e967c85831813
mjschimm <mjschimm@gmail.com>
2019-03-14 20:21:07 -0700

    Lightweight form validation using Bootstrap/JQuery

--- js/test.js
+++ js/test.js
@@ -1,6 +1,24 @@
 var team = $('.team').offset().top;
 $(window).scroll(function() {
-    if (team - $(window).scrollTop() < 450) {
+    if (team - $(window).scrollTop() < 500) {
         $('.team-members').addClass('animate-team-members');
     }
-})
+});
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
